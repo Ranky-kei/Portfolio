9fb59b19ad01d7c96b6e6f6de5d41bbfe88f9e1c
Ranky-kei <141964463+Ranky-kei@users.noreply.github.com>
2024-07-04 00:57:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'dark';
+
+document.body.classList.add(currentTheme);
+if (currentTheme === 'dark') {
+    themeToggle.checked = true;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const currentTheme = localStorage.getItem('theme') || 'dark';
-
-    document.body.classList.add(currentTheme);
-    if (currentTheme === 'dark') {
-        themeToggle.checked = true;
-    }
-
     themeToggle.addEventListener('change', () => {
         if (themeToggle.checked) {
             document.body.classList.replace('light', 'dark');
