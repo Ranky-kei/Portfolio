e078184cfb33c28be6898f94111854a4ea013774
Ranky-kei <141964463+Ranky-kei@users.noreply.github.com>
2024-07-05 00:12:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const themeToggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme') || 'dark';
-
-document.body.classList.add(currentTheme);
-if (currentTheme === 'dark') {
-    themeToggle.checked = true;
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme') || 'dark';
+    
+    document.body.classList.add(currentTheme);
+    themeToggle.checked = currentTheme === 'dark';
+
     themeToggle.addEventListener('change', () => {
-        if (themeToggle.checked) {
+        const newTheme = themeToggle.checked ? 'dark' : 'light';
+        if (newTheme === "dark") {
             document.body.classList.replace('light', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
+        } else if (newTheme === "light") {
             document.body.classList.replace('dark', 'light');
-            localStorage.setItem('theme', 'light');
         }
+        localStorage.setItem('theme', newTheme);
     });
 });
